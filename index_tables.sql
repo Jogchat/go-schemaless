0e9f2aacc9e6bf9373cb50a3ba49fc383f7044b5
Chaoran Wang <chaoran@uber.com>
2018-06-10 18:54:05 -0700

    Create index_tables.sql

---
+++ index_tables.sql
@@ -0,0 +1,30 @@
+/*
+CREATE TABLE users(
+   id BINARY(16) PRIMARY KEY,
+   username TEXT,
+   email TEXT,
+   phone INT(10),
+   password TEXT,
+   activate boolean
+);
+*/
+
+CREATE TABLE index_users_id(
+    id BINARY(16) NOT NULL,
+    row_key BINARY(16) NOT NULL UNIQUE,
+    PRIMARY KEY (id, row_key)
+);
+
+CREATE TABLE companies(
+   id BINARY(16) PRIMARY KEY,
+   category TEXT,
+   domain TEXT,
+   name TEXT
+);
+
+CREATE TABLE schools(
+   id BINARY(16) PRIMARY KEY,
+   category TEXT,
+   domain TEXT,
+   name TEXT
+);
