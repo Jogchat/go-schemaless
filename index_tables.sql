616ebe822478df0b62c95c406bb4743ee3e759c9
Chaoran Wang <chaoran@uber.com>
2018-06-10 18:56:55 -0700

    Update index_tables.sql

--- index_tables.sql
+++ index_tables.sql
@@ -13,18 +13,23 @@ CREATE TABLE index_users_id(
     id BINARY(16) NOT NULL,
     row_key BINARY(16) NOT NULL UNIQUE,
     PRIMARY KEY (id, row_key)
-);
+) ENGINE=InnoDB;
 
+/*
 CREATE TABLE companies(
    id BINARY(16) PRIMARY KEY,
    category TEXT,
    domain TEXT,
    name TEXT
 );
+*/
 
+
+/*
 CREATE TABLE schools(
    id BINARY(16) PRIMARY KEY,
    category TEXT,
    domain TEXT,
    name TEXT
 );
+*/
