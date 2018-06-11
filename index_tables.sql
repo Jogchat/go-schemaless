3a7a952a14943b6f11cc41ae28640f829655c8bf
Chaoran Wang <chaoran@uber.com>
2018-06-11 08:39:07 -0700

    Update index_tables.sql

--- index_tables.sql
+++ index_tables.sql
@@ -1,3 +1,17 @@
+
+/*
+CREATE TABLE cell
+(
+    added_at         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    row_key          BINARY(16) NOT NULL,
+    column_name      VARCHAR(64) NOT NULL,
+    ref_key          BIGINT NOT NULL,
+    body             BLOB,
+    created_at       DATETIME DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT cell_idx UNIQUE(row_key, column_name,ref_key)
+) ENGINE=InnoDB;
+*/
+
 /*
 CREATE TABLE users(
    id BINARY(16) PRIMARY KEY,
@@ -15,30 +29,37 @@ CREATE TABLE index_users_id(
     PRIMARY KEY (id, row_key)
 ) ENGINE=InnoDB;
 
-
-CREATE TABLE index_users_id(
+CREATE TABLE index_users_username(
     username TEXT NOT NULL,
     row_key BINARY(16) NOT NULL UNIQUE,
-    PRIMARY KEY (id, row_key)
+    PRIMARY KEY (username, row_key)
 ) ENGINE=InnoDB;
 
-CREATE TABLE index_users_id(
-    id BINARY(16) NOT NULL,
+CREATE TABLE index_users_email(
+    email TEXT NOT NULL,
     row_key BINARY(16) NOT NULL UNIQUE,
-    PRIMARY KEY (id, row_key)
+    PRIMARY KEY (email, row_key)
 ) ENGINE=InnoDB;
 
-CREATE TABLE index_users_id(
-    id BINARY(16) NOT NULL,
+CREATE TABLE index_users_phone(
+    phone INT(10),
     row_key BINARY(16) NOT NULL UNIQUE,
-    PRIMARY KEY (id, row_key)
+    PRIMARY KEY (phone, row_key)
 ) ENGINE=InnoDB;
 
-CREATE TABLE index_users_id(
-    id BINARY(16) NOT NULL,
+CREATE TABLE index_users_password(
+    password TEXT NOT NULL,
     row_key BINARY(16) NOT NULL UNIQUE,
-    PRIMARY KEY (id, row_key)
+    PRIMARY KEY (password, row_key)
 ) ENGINE=InnoDB;
+
+CREATE TABLE index_users_activate(
+    activate BOOLEAN NOT NULL,
+    row_key BINARY(16) NOT NULL UNIQUE,
+    PRIMARY KEY (password, row_key)
+) ENGINE=InnoDB;
+
+
 /*
 CREATE TABLE companies(
    id BINARY(16) PRIMARY KEY,
