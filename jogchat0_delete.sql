1ca7b222e5a230f35b5eb2e2a110b0f1174a4901
Chaoran Wang <superchaoran@gmail.com>
2018-06-26 00:07:16 -0700

    Create jogchat0_delete.sql

---
+++ jogchat0_delete.sql
@@ -0,0 +1,24 @@
+use jogchat0;
+
+DELETE from cell;
+DELETE from users;
+DELETE from index_users_id;
+DELETE from index_users_username;
+DELETE from index_users_email;
+DELETE from index_users_phone;
+DELETE from index_users_password;
+DELETE from index_users_activate;
+DELETE from index_users_token;
+
+DELETE from companies;
+DELETE from index_companies_id;
+DELETE from index_companies_category;
+DELETE from index_companies_domain;
+DELETE from index_companies_name;
+
+
+DELETE from schools;
+DELETE from index_schools_id;
+DELETE from index_schools_category;
+DELETE from index_schools_domain;
+DELETE from index_schools_name;DELETE from scho
