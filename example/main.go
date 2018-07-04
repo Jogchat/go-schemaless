e912876d0e6d564d29ba609a67d1853288874e2e
Mengxiong <mliu60@illinois.edu>
2018-07-04 16:27:41 -0700

    schemaless test

--- example/main.go
+++ example/main.go
@@ -13,12 +13,11 @@ import (
 )
 
 
-func newBusiness(id uuid.UUID, colKey string, category string, domain string, name string) models.Cell {
+func newBusiness(id uuid.UUID, colKey string, domain string, name string) models.Cell {
 	rowKey := utils.NewUUID().Bytes()
 	refKey := time.Now().UnixNano()
 	blob, err := json.Marshal(map[string]interface{} {
 		"id": id,
-		"category": category,
 		"domain": domain,
 		"name": name,
 	})
@@ -30,22 +29,22 @@ func main() {
 	dataStore := schemaless.InitDataStore()
 	defer dataStore.Destroy(context.TODO())
 
-	UIUC := newBusiness(utils.NewUUID(), "schools", "university", "illinois.edu", "UIUC")
+	UIUC := newBusiness(utils.NewUUID(), "schools", "illinois.edu", "UIUC")
 	err := dataStore.PutCell(context.TODO(), UIUC.RowKey, UIUC.ColumnName, UIUC.RefKey, UIUC)
 	utils.CheckErr(err)
 
-	CMU := newBusiness(utils.NewUUID(), "schools", "university", "andrew.cmu.edu", "CMU")
+	CMU := newBusiness(utils.NewUUID(), "schools", "andrew.cmu.edu", "CMU")
 	err = dataStore.PutCell(context.TODO(), CMU.RowKey, CMU.ColumnName, CMU.RefKey, CMU)
 
-	Sift := newBusiness(utils.NewUUID(), "companies", "technology", "siftscience.com", "Sift Science")
+	Sift := newBusiness(utils.NewUUID(), "companies", "siftscience.com", "Sift Science")
 	err = dataStore.PutCell(context.TODO(), Sift.RowKey, Sift.ColumnName, Sift.RefKey, Sift)
 	utils.CheckErr(err)
 
-	Yahoo := newBusiness(utils.NewUUID(), "companies", "technology", "yahoo-inc.com", "Yahoo!")
+	Yahoo := newBusiness(utils.NewUUID(), "companies", "yahoo-inc.com", "Yahoo!")
 	err = dataStore.PutCell(context.TODO(), Yahoo.RowKey, Yahoo.ColumnName, Yahoo.RefKey, Yahoo)
 	utils.CheckErr(err)
 
-	cells, _, err := dataStore.GetCellsByFieldLatest(context.TODO(), "schools", "category", "university")
+	cells, _, err := dataStore.GetCellsByFieldLatest(context.TODO(), "schools", "domain", "illinois.edu")
 	utils.CheckErr(err)
 	for _, cell := range cells {
 		fmt.Println(cell.String())
